24aac9d6e510c54d7df3b74689b3ffa142ac0ed7
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:15:05 +0200

    TimeZone

--- src/com/cfe/Configuration.java
+++ src/com/cfe/Configuration.java
@@ -1,14 +1,22 @@
 package com.cfe;
 
+import java.util.TimeZone;
+
 public class Configuration {
 	private final Language language;
+	private final TimeZone timeZone;
 
-	public Configuration(Language language) {
+	public Configuration(Language language, TimeZone timeZone) {
 		this.language = language;
+		this.timeZone = timeZone;
 	}
 
 	public Language getLanguage() {
 		return language;
+	}
+
+	public TimeZone getTimeZone() {
+		return timeZone;
 	}
 
 	public enum Language {
