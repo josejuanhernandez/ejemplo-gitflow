a2c34336d05acec56fdaa937efff611e789ccf7b
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:25:03 +0200

    Fecha sin guiones

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world! " + Instant.now().toString().substring(0, 10));
+		String fecha = Instant.now().toString().substring(0, 10).replace("-","");
+		System.out.println("Hello world! " + fecha);
 	}
 }
