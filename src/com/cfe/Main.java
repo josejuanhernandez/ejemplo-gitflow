1126a7d8f44e83c9bec80d1139f190a1a6557457
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:18:57 +0200

    Mostrando la configuracion

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -9,6 +9,7 @@ import static java.util.TimeZone.getTimeZone;
 public class Main {
 	public static void main(String[] args) {
 		Configuration configuration = new Configuration(Spanish, getTimeZone("GMT"));
+		System.out.println(configuration.getLanguage());
 		System.out.println("Hello world!" + Instant.now());
 	}
 }
