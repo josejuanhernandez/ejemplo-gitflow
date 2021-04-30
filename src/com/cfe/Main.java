7aa631e69e4de226b0ac7317274e5e8e3bd68ccd
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:19:44 +0200

    Mostrando hola mundo

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -5,5 +5,6 @@ import java.time.Instant;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world! " + Instant.now().toString().substring(0, 10));
+		System.out.println("¡Hola mundo! " + Instant.now().toString().substring(0, 10));
 	}
 }
