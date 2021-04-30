5777141fb1fcc6b60a623fd6dc3a8c54c3c2d846
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:01:45 +0200

    Main arreglando espacios

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -4,6 +4,6 @@ import java.time.Instant;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!" + Instant.now());
+		System.out.println("Hello world! " + Instant.now());
 	}
 }
