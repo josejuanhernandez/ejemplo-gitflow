1ecc30e175975839f5283aabb7a1634e2cd0dee8
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:17:35 +0200

    Usando configuration en Main

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -1,9 +1,14 @@
 package com.cfe;
 
 import java.time.Instant;
+import java.util.TimeZone;
+
+import static com.cfe.Configuration.Language.Spanish;
+import static java.util.TimeZone.getTimeZone;
 
 public class Main {
 	public static void main(String[] args) {
+		Configuration configuration = new Configuration(Spanish, getTimeZone("GMT"));
 		System.out.println("Hello world!" + Instant.now());
 	}
 }
