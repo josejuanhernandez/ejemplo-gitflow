5027a01855459381b81c6a932db9e4a124adcd0b
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:03:46 +0200

    Configuration reformateado

--- src/com/cfe/Configuration.java
+++ src/com/cfe/Configuration.java
@@ -1,2 +1,19 @@
-package com.cfe;public class Configuration {
+package com.cfe;
+
+public class Configuration {
+	private final Language language;
+
+	public Configuration(Language language) {
+		this.language = language;
+	}
+
+	public Language getLanguage() {
+		return language;
+	}
+
+	public enum Language {
+		Spanish, English
+	}
+
+
 }
