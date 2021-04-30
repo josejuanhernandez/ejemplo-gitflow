fd0086822713740d0c95820d0ab61bc97a9a412d
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:00:29 +0200

    Instant now en el Main

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -1,7 +1,9 @@
 package com.cfe;
 
+import java.time.Instant;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world!" + Instant.now());
 	}
 }
