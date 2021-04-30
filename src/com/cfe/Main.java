2f7e3a9ceb987b2e88bb23dfe621fc4717db931c
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-30 16:20:31 +0200

    Refactor de la fecha

--- src/com/cfe/Main.java
+++ src/com/cfe/Main.java
@@ -4,7 +4,8 @@ import java.time.Instant;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world! " + Instant.now().toString().substring(0, 10));
-		System.out.println("¡Hola mundo! " + Instant.now().toString().substring(0, 10));
+		String fecha = Instant.now().toString().substring(0, 10);
+		System.out.println("Hello world! " + fecha);
+		System.out.println("¡Hola mundo! " + fecha);
 	}
 }
